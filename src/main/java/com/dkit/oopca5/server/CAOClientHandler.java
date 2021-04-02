353c63a87e048169f716e4b5aa46c4b9244d3009
D00228000 <D00228000@student.dkit.ie>
2021-04-02 16:42:02 +0100

    Implemented threaded clients
    I managed to implement threading. Now multiple clients can access the
    server without having to wait until someone else has finished using the
    CAO service.

--- src/main/java/com/dkit/oopca5/server/CAOClientHandler.java
+++ src/main/java/com/dkit/oopca5/server/CAOClientHandler.java
@@ -4,6 +4,113 @@ package com.dkit.oopca5.server;
 The CAOClientHandler will run as a thread. It should listen for messages from the Client and respond to them.There should be one CAOClientHandler per Client.
  */
 
-public class CAOClientHandler
+import com.dkit.oopca5.core.CAOService;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class CAOClientHandler extends Thread
 {
+    private Socket dataSocket;
+    private Scanner input;
+    private PrintWriter output;
+    private int number;
+
+    public CAOClientHandler(ThreadGroup group, String name, Socket dataSocket, int number)
+    {
+        super(group,name);
+        try
+        {
+            this.dataSocket = dataSocket;
+            this.number = number;
+            input = new Scanner(new InputStreamReader(this.dataSocket.getInputStream()));
+            output = new PrintWriter(this.dataSocket.getOutputStream());
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void run() {
+        //custom run method
+        String incomingMessage = "";
+        String response = "";
+
+        try
+        {
+            //The server should run forever hence use a while
+            while (true)
+            {
+                response = null;
+                //Take the information from the client
+                incomingMessage = input.nextLine();
+                System.out.println("Received message: " + incomingMessage);
+
+                String[] messageComponents = incomingMessage.split(CAOService.BREAKING_CHARACTER);
+
+                if (messageComponents[0].equalsIgnoreCase(CAOService.END_SESSION))
+                {
+                    response = "Quit";
+                }
+                else if (messageComponents[0].equalsIgnoreCase(CAOService.REGISTER_COMMAND))
+                {
+                    response = CAOService.SUCCESSFUL_REGISTER;
+                }
+                else if (messageComponents[0].equalsIgnoreCase(CAOService.ATTEMPT_LOGIN))
+                {
+                    //getLogin();
+                    response = CAOService.SUCCESSFUL_LOGIN;
+                }
+                else if (messageComponents[0].equalsIgnoreCase(CAOService.LOG_OUT))
+                {
+                    response = "LOGGED OUT";
+                }
+                else if (messageComponents[0].equalsIgnoreCase(CAOService.DISPLAY_COURSE))
+                {
+                    response = "DISPLAY COURSE";
+                }
+                else if (messageComponents[0].equalsIgnoreCase(CAOService.DISPLAY_ALL_COURSES))
+                {
+                    response = "DISPLAY ALL COURSES";
+                }
+                else if (messageComponents[0].equalsIgnoreCase(CAOService.DISPLAY_CURRENT_CHOICES))
+                {
+                    response = "DISPLAY CURRENT CHOICES";
+                }
+                else if (messageComponents[0].equalsIgnoreCase(CAOService.UPDATE_CURRENT_CHOICES))
+                {
+                    response = "UPDATE CURRENT CHOICES";
+                }
+                else
+                {
+                    response = CAOService.UNKNOWN;
+                }
+                //Gives out response
+                output.println(response);
+                output.flush();
+            }
+        }
+        catch (NoSuchElementException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        finally
+        {
+            try
+            {
+                System.out.println("The connection has closed between the server and the client #"+number);
+                dataSocket.close();
+            }
+            catch (IOException e)
+            {
+                System.out.println("The Client #"+number+" is unable to disconnect "+e.getMessage());
+                System.exit(1);//closes the system down
+            }
+        }
+    }
 }
--- src/main/java/com/dkit/oopca5/server/CAOServer.java
+++ src/main/java/com/dkit/oopca5/server/CAOServer.java
@@ -6,105 +6,47 @@ package com.dkit.oopca5.server;
  */
 
 import com.dkit.oopca5.core.CAOService;
-import com.dkit.oopca5.core.Student;
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
 
 public class CAOServer
 {
-
     public static void main(String[] args)
     {
         try
         {
+            //creating a tcp socket
             ServerSocket listeningSocket = new ServerSocket(CAOService.LISTENING_ON_PORT);
-            Socket dataSocket = new Socket();
+            //Socket dataSocket = new Socket();
 
+            //creating a threadGroup
+            ThreadGroup clientThreadGroup = new ThreadGroup("Client thread group");
+
+            //set the thread priority so more clients are accepted to the system
+            clientThreadGroup.setMaxPriority(Thread.currentThread().getPriority()-1);
+
+            //server information below
             boolean continueRunning = true;
+            int threadClientCount = 0;
             while (continueRunning)
             {
                 System.out.println("Sever is listening on port: " + CAOService.LISTENING_ON_PORT);
-                dataSocket = listeningSocket.accept();
+                Socket dataSocket = listeningSocket.accept();
 
-                //Creating the input and output streams
-                OutputStream outputStream = dataSocket.getOutputStream();
-                PrintWriter output = new PrintWriter(new OutputStreamWriter(outputStream));
-                InputStream inputStream = dataSocket.getInputStream();
-                Scanner input = new Scanner(new InputStreamReader(inputStream));
-                //Keyboard and message declarations
-                String incomingMessage = "";
-                String response;
+                threadClientCount++;
+                System.out.println("The server has accepted "+threadClientCount+" clients to the server system");
 
-                //The server should run forever hence use a while
-                while (true)
-                {
-                    response = null;
-                    //Take the information from the client
-                    incomingMessage = input.nextLine();
-                    System.out.println("Received message: " + incomingMessage);
-
-                    String[] messageComponents = incomingMessage.split(CAOService.BREAKING_CHARACTER);
-
-                    if(messageComponents[0].equalsIgnoreCase(CAOService.END_SESSION))
-                    {
-                        response = "Quit";
-                    }
-                    else if (messageComponents[0].equalsIgnoreCase(CAOService.REGISTER_COMMAND))
-                    {
-                       response = CAOService.SUCCESSFUL_REGISTER;
-                    }
-                    else if(messageComponents[0].equalsIgnoreCase(CAOService.ATTEMPT_LOGIN))
-                    {
-                        //getLogin();
-                        response = CAOService.SUCCESSFUL_LOGIN;
-                    }
-                    else if(messageComponents[0].equalsIgnoreCase(CAOService.LOG_OUT))
-                    {
-                        response = "LOGGED OUT";
-                    }
-                    else if(messageComponents[0].equalsIgnoreCase(CAOService.DISPLAY_COURSE))
-                    {
-                        response = "DISPLAY COURSE";
-                    }
-                    else if(messageComponents[0].equalsIgnoreCase(CAOService.DISPLAY_ALL_COURSES))
-                    {
-                        response = "DISPLAY ALL COURSES";
-                    }
-                    else if(messageComponents[0].equalsIgnoreCase(CAOService.DISPLAY_CURRENT_CHOICES))
-                    {
-                        response = "DISPLAY CURRENT CHOICES";
-                    }
-                    else if(messageComponents[0].equalsIgnoreCase(CAOService.UPDATE_CURRENT_CHOICES))
-                    {
-                        response = "UPDATE CURRENT CHOICES";
-                    }
-                    else
-                    {
-                        response = CAOService.UNKNOWN;
-                    }
-
-                    output.println(response);
-                    output.flush();
-                }
+                //creating the thread with its group and other needed items
+                CAOClientHandler newClient = new CAOClientHandler(clientThreadGroup,dataSocket.getInetAddress()+"",dataSocket,threadClientCount);
+                newClient.start();
             }
-            dataSocket.close();
             listeningSocket.close();
         }
         catch (IOException e)
         {
             System.out.println(e.getMessage());
         }
-        catch (NoSuchElementException e)
-        {
-            System.out.println("A client has disconnected from the server");
-        }
-
     }
-
-
 }
 
